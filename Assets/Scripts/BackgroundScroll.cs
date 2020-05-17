2d07071d8c8dc21e99fa54c949ba7fc7faa924c2
Shiroi <fabian_frontczak@wp.pl>
2020-05-17 23:59:03 +0200

    Dodano przesuwające się tło

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    public float ScrollSpeed = 5f;
+    public float DistanceLimit = 34f;
+    public GameObject Player;
+
+    void Start()
+    {
+        //_startPos = transform.position;
+    }
+
+    void Update()
+    {
+        float myPosY = transform.position.y;
+        float playerPosY = Player.transform.position.y;
+
+        if (myPosY < playerPosY && Mathf.Abs(myPosY - playerPosY) > DistanceLimit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 3 * DistanceLimit, transform.position.z);
+        }
+
+        transform.position = new Vector3(transform.position.x, transform.position.y - Time.deltaTime * ScrollSpeed, transform.position.z);
+
+        //float newPos = Mathf.Repeat(Time.time * ScrollSpeed, 35);
+        //transform.position = _startPos + Vector2.down * newPos;
+    }
+}
