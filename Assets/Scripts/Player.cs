27051f16c5388bf7c18c0dbcf2d7a83ba7927997
Shiroi <fabian_frontczak@wp.pl>
2020-05-18 22:21:47 +0200

    Dodano odchylanie w lewo / prawo
    Poprzez kliknięcie w lewą lub prawą część ekranu.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 _startingPos;
+    private Quaternion _startingRot;
+
     void Start()
     {
-        
+        Input.multiTouchEnabled = false;
+        _startingPos = transform.position;
+        _startingRot = transform.rotation;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (touch.position.x < Screen.width / 2)
+                {
+                    transform.position = new Vector3(_startingPos.x - 0.3f, _startingPos.y, _startingPos.z);
+                    transform.rotation = new Quaternion(_startingRot.x, _startingRot.y + 70.0f, _startingRot.z, _startingRot.w);
+                }
+                else if (touch.position.x > Screen.width / 2)
+                {
+                    transform.position = new Vector3(_startingPos.x + 0.3f, _startingPos.y, _startingPos.z);
+                    transform.rotation = new Quaternion(_startingRot.x, _startingRot.y - 70.0f, _startingRot.z, _startingRot.w);
+
+                }
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                transform.position = _startingPos;
+                transform.rotation = _startingRot;
+            }
+        }
     }
 }
