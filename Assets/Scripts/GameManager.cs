d7fc0bbed3b35f600d77ce696d2606ce3ccd3207
Shiroi <fabian_frontczak@wp.pl>
2020-05-31 01:06:20 +0200

    Dodano UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,31 +7,76 @@ public class GameManager : MonoBehaviour
 {
     public float ScrollSpeed { get; set; }
     public int PointsCounter { get => _pointsCounter; set => _pointsCounter += value; }
+    public float RemainingTime { get; set; }
 
+    [SerializeField] private UiManager _uiManager;
+    [SerializeField] private GameObject _playerObject;
+    [SerializeField] private GameObject _obstacles;
     [SerializeField] private float _startSpeed = 5f;
     [SerializeField] private float _speedLimit = 10f;
-    [SerializeField] private Player _player;
+    [SerializeField] private float _timeLimitMin = 1;
+    private Player _player;
+    private float _currentTime;
+
+    private GameState _gameState;
     private int _pointsCounter;
 
     void Start()
     {
         _pointsCounter = 0;
         ScrollSpeed = _startSpeed;
+        _gameState = GameState.MENU;
+        _uiManager.ShowStartUi();
+        _currentTime = _timeLimitMin * 60.0f;
+        _uiManager.SetRemainingTime(_currentTime);
     }
 
     void Update()
     {
+        if (_gameState == GameState.IN_PROGRESS &&_currentTime > 0)
+        {
+            _currentTime -= Time.deltaTime;
+            _uiManager.SetRemainingTime(_currentTime);
+        }
+        else
+            _uiManager.SetRemainingTime(0);
     }
 
 
     private void FixedUpdate()
     {
-        if(!_player.IsStunned && _startSpeed < _speedLimit)
-            ScrollSpeed += 0.001f;
+        if (_player != null)
+        {
+            if (!_player.IsStunned && _startSpeed < _speedLimit)
+                ScrollSpeed += 0.001f;
+        }
     }
 
     public void RestartSpeed()
     {
         ScrollSpeed = _startSpeed;
     }
+
+    public void StartGame()
+    {
+        _gameState = GameState.IN_PROGRESS;
+        _uiManager.HideStartUi();
+        _uiManager.ShowInGameUi();
+        _playerObject.SetActive(true);
+        _player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        _obstacles.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+
+    }
+}
+
+public enum GameState
+{
+    MENU = 0,
+    IN_PROGRESS = 1,
+    COMPLETED = 2,
+    GAME_OVER = 3
 }
--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -8,6 +8,11 @@ public class UiManager : MonoBehaviour
 {
     [SerializeField] private GameManager _gameManager;
     [SerializeField] private TextMeshProUGUI _pointsText;
+    [SerializeField] private TextMeshProUGUI _remainingTimeText;
+    [SerializeField] private Image _startButtonIcon;
+    [SerializeField] private Image _retryButtonIcon;
+    [SerializeField] private Image _gameOverIcon;
+    [SerializeField] private Image _successIcon;
 
     void Start()
     {
@@ -17,5 +22,58 @@ public class UiManager : MonoBehaviour
     void Update()
     {
         _pointsText.text = _gameManager.PointsCounter.ToString();
+    }
+
+    public void ShowStartUi()
+    {
+        _startButtonIcon.gameObject.SetActive(true);
+    }
+
+    public void HideStartUi()
+    {
+        _startButtonIcon.gameObject.SetActive(false);
+    }
+
+    public void ShowInGameUi()
+    {
+        _startButtonIcon.gameObject.SetActive(false);
+        _gameOverIcon.gameObject.SetActive(false);
+        _retryButtonIcon.gameObject.SetActive(false);
+        _pointsText.gameObject.SetActive(true);
+        _remainingTimeText.gameObject.SetActive(true);
+    }
+
+    public void HideInGameUi()
+    {
+        _pointsText.gameObject.SetActive(false);
+        _remainingTimeText.gameObject.SetActive(false);
+    }
+
+    public void ShowGameOverUi()
+    {
+        _gameOverIcon.gameObject.SetActive(true);
+        _retryButtonIcon.gameObject.SetActive(true);
+    }
+
+    public string FormatTime(float timeSec)
+    {
+        string result = "";
+        int minutes = (int)timeSec / 60;
+        result += minutes.ToString() + ":";
+        int seconds = (int)timeSec - minutes * 60;
+        if (seconds < 10)
+        {
+            result += "0";
+        }
+        result += seconds.ToString();
+        return result;
+    }
+
+    public void SetRemainingTime(float time)
+    {
+        if (time <= 0)
+            _remainingTimeText.text = FormatTime(0);
+        else
+            _remainingTimeText.text = FormatTime(time);
     }
 }
