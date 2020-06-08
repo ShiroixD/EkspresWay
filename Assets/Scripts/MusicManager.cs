1d5c04797271fae5c1e3498881c0f6559728c4de
Shiroi <fabian_frontczak@wp.pl>
2020-06-09 00:52:34 +0200

    Muzyka w tle, stun
    Dodano muzykę w tle, dźwięki dla stuna, odblokowywania stuna oraz uwolnienia się z niego

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] musicClips;
+
+    void Start()
+    {;
+        int number = Random.Range(0, musicClips.Length);
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = musicClips[number];
+        audioSource.Play();
+    }
+}
